dc796f73c1e9b9e0defc48f6b2c1abe149ad4c0e
Nathan.Gomez <gomezdevlopment@gmail.com>
2022-03-02 18:40:56 -0500

    Create navigation bar and fragments

--- app/src/main/java/com/lifepoint/calculator/MainActivity.kt
+++ app/src/main/java/com/lifepoint/calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lifepoint.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(nav, navController)
     }
 }
