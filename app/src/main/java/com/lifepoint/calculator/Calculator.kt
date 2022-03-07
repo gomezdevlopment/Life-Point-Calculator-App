af866767324f3283d97c925c240a1b71d5ab73ad
Nathan.Gomez <gomezdevlopment@gmail.com>
2022-03-07 15:51:09 -0500

    Clean up fragments code

--- app/src/main/java/com/lifepoint/calculator/Calculator.kt
+++ app/src/main/java/com/lifepoint/calculator/Calculator.kt
@@ -15,30 +15,13 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 private var sound: MediaPlayer? = null
 private var soundFX: Boolean = true
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Calculator.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Calculator : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -196,22 +179,5 @@ class Calculator : Fragment() {
     companion object {
         var playerOneLifePointTotal: String = "8000"
         var playerTwoLifePointTotal: String = "8000"
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment calculator.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Calculator().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
